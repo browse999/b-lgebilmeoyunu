1e025fe996b226e37bcaf35014590a126d842c07
Browser <92232900+browse999@users.noreply.github.com>
2024-08-16 20:13:36 +0300

    Add files via upload

---
+++ bolgeBilmeYarismasi.js
@@ -0,0 +1,33 @@
+let marmara ,icAnadolu,ege, ankara , istanbul , izmir
+
+izmir = ege
+
+ankara = icAnadolu
+
+istanbul = marmara
+
+ soru1 = prompt(`izmir hangi bölgededir 1-İc Anadolu 2-Ege 3-Marmara`)
+
+
+ if (soru1==2){
+
+    alert('Tebrikler doğru cevap sıradaki soru')
+
+    soru2 =prompt(`Ankara hangi bölgededir ? 1-İc Anadolu 2-Ege 3-Marmara `)
+
+        if(soru2==1) {
+
+            alert('Tebrikler yarısmayı kazandınız')
+        }
+
+        else{
+            alert('Yarismadan elendiniz.')
+        }
+ }
+
+
+ else (
+
+    alert('Yarismadan Elendiniz !')
+ )
+
